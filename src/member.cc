b570d3e80961e242af22be5ec09e169f1eef3d32
Morgan Rockett <rockett.m@husky.neu.edu>
2018-04-20 06:19:37 +0000

    prog member.cc

--- src/member.cc
+++ src/member.cc
@@ -18,7 +18,32 @@ void Member::DumpConnections() {
 
 void Member::PathToMemberBFS(uint64_t dst_member_id) {
   // Fill in your code here.
+  this->color = COLOR_GRAY;
+  this->parent = NULL;
+  std::queue<Member*> q;
+  q.push(this);
+  while(!q.empty()) {
+    Member* m = q.front();
+    q.pop;
+    for (auto conn_mem : m->connecting_members) {
+      auto mryconn = conn_mem.second;
+      auto v = mryconn = mryconn.dst;
+      if (v->member_id == dst.member_id) {
+        v->parent = m;
+        PrintPath(v);
+        return;
+      }
+      if (v->color == COLOR_WHITE){
+        v->color = COLOR_GRAY;
+        v->parent = m;
+        q.push(v);
+      }
+    }
+    m->color = COLOR_BLACK;
+    }
+  }
   
+  void 
   for (auto i = begin(members); i < end(members); i++){
     for (auto j = std:next(i); j < end(members); j++){
       auto m1 = * i;
